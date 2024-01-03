f95437042d017caca2ef7a6925e15219731580fe
Jamie Wu <jamie.wu@dotcomintl.com>
2024-01-03 18:04:53 +0800

    feat: docker deploymentfeat: docker deployment

--- admin-ui/nuxt.config.ts
+++ admin-ui/nuxt.config.ts
@@ -1,7 +1,18 @@
+import { joinURL } from "ufo";
+
+const API_URL = process.env.API_URL
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
-    port: 5073
-  }
-})
+    port: 5073,
+  },
+  routeRules: {
+    "/api/**": API_URL
+      ? {
+          proxy: joinURL(API_URL, "/**"),
+        }
+      : {},
+  },
+});
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 compose_file=docker-compose.prod.yml
-service_name=backend
+# service_name=backend
 
-sha=$(git rev-parse --short HEAD)
-old_container_ids=$(docker-compose -f $compose_file ps -q $service_name)
-replicas=`echo $old_container_ids | wc -l`
+mode=${1:-all}
+if [ $mode == 'backend' ] || [ $mode == 'admin' ]
+then
+  echo 'Deploy' $mode
+else
+  echo 'Deploy All' 
+  mode='backend admin'
+fi
+
 
 reload_nginx() {  
   # docker exec nginx /usr/sbin/nginx -s reload  
@@ -20,40 +26,51 @@ else
   echo "nginx is started."
 fi
 
-# bring a new container online, running new code  
-# (nginx continues routing to the old container only)  
-TAG=$sha \
-docker-compose -f $compose_file up \
-  -d \
-  --no-deps \
-  --scale $service_name=$(expr 2 \* $replicas) \
-  --no-recreate \
-  --build \
-  $service_name
+deploy() {
+  service_name=$1
+  sha=$(git rev-parse --short HEAD)
+  old_container_ids=$(docker-compose -f $compose_file ps -q $service_name)
+  replicas=`echo $old_container_ids | wc -l`
+
+  # bring a new container online, running new code  
+  # (nginx continues routing to the old container only)  
+  TAG=$sha \
+  docker-compose -f $compose_file up \
+    -d \
+    --no-deps \
+    --scale $service_name=$(expr 2 \* $replicas) \
+    --no-recreate \
+    --build \
+    $service_name
 
 
-# start routing requests to the new container (as well as the old)
-reload_nginx
+  # start routing requests to the new container (as well as the old)
+  reload_nginx
 
-# # Not working for `replicated` mode, cause of there are no ports exposed
-# # wait for new container to be available  
-# new_container_id=$(docker ps -f name=$service_name -q | head -n1)
-# new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
-# curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
+  # # Not working for `replicated` mode, cause of there are no ports exposed
+  # # wait for new container to be available  
+  # new_container_id=$(docker ps -f name=$service_name -q | head -n1)
+  # new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
+  # curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
 
 
-# take the old container offline  
-if [ -n $old_container_ids ]
-then
-  docker stop $old_container_ids
-  docker rm $old_container_ids
-fi
+  # take the old container offline  
+  if [ -n $old_container_ids ]
+  then
+    docker stop $old_container_ids
+    docker rm $old_container_ids
+  fi
 
-docker-compose -f $compose_file up \
-  -d \
-  --no-deps \
-  --scale $service_name=$(expr 1 \* $replicas) \
-  --no-recreate \
-  $service_name
+  docker-compose -f $compose_file up \
+    -d \
+    --no-deps \
+    --scale $service_name=$(expr 1 \* $replicas) \
+    --no-recreate \
+    $service_name
 
-reload_nginx
+  reload_nginx
+}
+
+for item in $mode; do
+  deploy $item
+done
